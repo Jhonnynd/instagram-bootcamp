14ee06dc80930c274c0b1704346e83af7d00340a
Jhonnynd <jhonnynd@gmail.com>
2023-06-26 17:04:48 +0800

    added hooks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { onChildAdded, push, ref, set } from "firebase/database";
 import { database, storage, auth } from "./firebase";
 import {
@@ -19,79 +19,70 @@ import "./App.css";
 const DB_MESSAGES_KEY = "messages";
 const STORAGE_KEY = "images/";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      messages: [],
-      messageInput: "",
-      date: "",
-      fileInputFile: null,
-      fileInputValue: "",
-      isLoggedIn: false,
-      user: {},
-      email: "",
-      password: "",
-    };
-  }
-  componentDidMount() {
+export default function App() {
+  const [messages, setMessages] = useState([]);
+  const [messageInput, setmessageInput] = useState("");
+  const [date, setdate] = useState("");
+  const [fileInputFile, setfileInputFile] = useState(null);
+  const [fileInputValue, setfileInputValue] = useState("");
+  const [isLoggedIn, setisLoggedIn] = useState(false);
+  const [user, setuser] = useState({});
+  const [email, setemail] = useState("");
+  const [password, setpassword] = useState("");
+
+  useEffect(() => {
     const messagesRef = ref(database, DB_MESSAGES_KEY);
     onChildAdded(messagesRef, (data) => {
-      this.setState((state) => ({
-        messages: [...state.messages, { key: data.key, val: data.val() }],
-      }));
+      setMessages((prevmessages) => [
+        ...prevmessages,
+        { key: data.key, val: data.val() },
+      ]);
     });
 
     onAuthStateChanged(auth, (user) => {
       if (user) {
         console.log(user);
-        this.setState({
-          isLoggedIn: true,
-          user: user,
-        });
+        setisLoggedIn(true);
+        setuser(user);
       } else {
-        this.setState({
-          isLoggedIn: false,
-          user: {},
-        });
+        setisLoggedIn(false);
+        setuser({});
       }
     });
-  }
+  }, []);
 
-  writeData = (url) => {
+  const writeData = (url) => {
     const messageListRef = ref(database, DB_MESSAGES_KEY);
     const newMessageRef = push(messageListRef);
 
     set(newMessageRef, {
-      email: this.state.email,
-      message: this.state.messageInput,
+      email: email,
+      message: messageInput,
       date: new Date().toLocaleString(),
       url: url,
     });
 
-    this.setState({
-      messageInput: "",
-      date: "",
-      fileInputFile: null,
-      fileInputValue: "",
-    });
+    setmessageInput("");
+    setdate("");
+    setfileInputFile(null);
+    setfileInputValue("");
   };
 
-  submit = () => {
+  const submit = () => {
     const fullStorageRef = storageRef(
       storage,
-      STORAGE_KEY + this.state.fileInputFile.name
+      STORAGE_KEY + fileInputFile.name
     );
 
-    uploadBytes(fullStorageRef, this.state.fileInputFile).then((snapshot) => {
+    uploadBytes(fullStorageRef, fileInputFile).then((snapshot) => {
       getDownloadURL(fullStorageRef).then((url) => {
-        this.writeData(url);
+        writeData(url);
       });
     });
   };
 
-  handleLogIn = () => {
-    signInWithEmailAndPassword(auth, this.state.email, this.state.password)
+  const handleLogIn = () => {
+    signInWithEmailAndPassword(auth, email, password)
       .then((userCred) => {
         console.log("logged in!");
         console.log(userCred.email);
@@ -101,107 +92,89 @@ class App extends React.Component {
       });
   };
 
-  handleSignIn = () => {
-    createUserWithEmailAndPassword(auth, this.state.email, this.state.password)
+  const handleSignIn = () => {
+    createUserWithEmailAndPassword(auth, email, password)
       .then((userCred) => {
         console.log("signed in!");
         console.log(userCred);
       })
       .catch((err) => console.log(err));
   };
-  handleSignOut = () => {
+  const handleSignOut = () => {
     signOut(auth).then(() => {
       console.log("Signed out :(");
     });
-    this.setState({
-      email: "",
-      password: "",
-    });
+    setemail("");
+    setpassword("");
   };
-  render() {
-    let messageListItems = this.state.messages.map((message) => (
-      <li key={message.key}>
-        <p>User: {message.val.email}</p>
-        <p>Message: {message.val.message}</p>
-        <p>Date: {message.val.date}</p>
-        <span>
-          {message.val.url ? (
-            <img
-              style={{ width: "30vw", height: "50vh", objectFit: "cover" }}
-              src={message.val.url}
-              alt={message.val.name}
+
+  let messageListItems = messages.map((message) => (
+    <li key={message.key}>
+      <p>User: {message.val.email}</p>
+      <p>Message: {message.val.message}</p>
+      <p>Date: {message.val.date}</p>
+      <span>
+        {message.val.url ? (
+          <img
+            style={{ width: "30vw", height: "50vh", objectFit: "cover" }}
+            src={message.val.url}
+            alt={message.val.name}
+          />
+        ) : (
+          "No img"
+        )}
+      </span>
+    </li>
+  ));
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+
+        {!isLoggedIn ? (
+          <>
+            <input
+              value={email}
+              name="email"
+              type="text"
+              placeholder="email"
+              onChange={(e) => setemail(e.target.value)}
             />
-          ) : (
-            "No img"
-          )}
-        </span>
-      </li>
-    ));
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+            <input
+              value={password}
+              name="password"
+              type="text"
+              placeholder="password"
+              onChange={(e) => setpassword(e.target.value)}
+            />
+            <button onClick={handleLogIn}>Login</button>
+            <button onClick={handleSignIn}>Sign Up</button>
+          </>
+        ) : (
+          <>
+            <button onClick={handleSignOut}>Sign out</button>
 
-          {!this.state.isLoggedIn ? (
-            <>
-              <input
-                value={this.state.email}
-                name="email"
-                type="text"
-                placeholder="email"
-                onChange={(e) =>
-                  this.setState({
-                    email: e.target.value,
-                  })
-                }
-              />
-              <input
-                value={this.state.password}
-                name="password"
-                type="text"
-                placeholder="password"
-                onChange={(e) =>
-                  this.setState({
-                    password: e.target.value,
-                  })
-                }
-              />
-              <button onClick={this.handleLogIn}>Login</button>
-              <button onClick={this.handleSignIn}>Sign Up</button>
-            </>
-          ) : (
-            <>
-              <button onClick={this.handleSignOut}>Sign out</button>
-
-              <input
-                placeholder="message"
-                type="text"
-                value={this.state.messageInput}
-                onChange={(e) =>
-                  this.setState({
-                    messageInput: e.target.value,
-                  })
-                }
-              ></input>
-              <input
-                value={this.state.fileInputValue}
-                type="file"
-                onChange={(e) =>
-                  this.setState({
-                    fileInputFile: e.target.files[0],
-                    fileInputValue: e.target.value,
-                  })
-                }
-              ></input>
-              <br />
-              <button onClick={this.submit}>Send</button>
-            </>
-          )}
-          <ul>{messageListItems}</ul>
-        </header>
-      </div>
-    );
-  }
+            <input
+              placeholder="message"
+              type="text"
+              value={messageInput}
+              onChange={(e) => setmessageInput(e.target.value)}
+            ></input>
+            <input
+              value={fileInputValue}
+              type="file"
+              onChange={(e) => {
+                setfileInputFile(e.target.files[0]);
+                setfileInputValue(e.target.value);
+              }}
+            ></input>
+            <br />
+            <button onClick={submit}>Send</button>
+          </>
+        )}
+        <ul>{messageListItems}</ul>
+      </header>
+    </div>
+  );
 }
-
-export default App;
